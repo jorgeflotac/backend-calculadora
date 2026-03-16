20bea0c96d92afa6c91ef0739d755020d8453f48
Jorge Flota <jorgeflota@MacBook-Air-M2-de-Jorge-3.local>
2026-03-15 18:44:50 -0600

    Backend calculadora funcionando

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://jorgeflotac_db_user:RTVIPv6NGOuq9kMQ@cluster0.vhpbhgn.mongodb.net/calculadora")
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.log(err));
+
+const operacionSchema = new mongoose.Schema({
+    num1: Number,
+    num2: Number,
+    operacion: String,
+    resultado: Number
+});
+
+const Operacion = mongoose.model("Operacion", operacionSchema);
+
+app.get("/operaciones", async (req, res) => {
+    const datos = await Operacion.find();
+    res.json(datos);
+});
+
+app.post("/operaciones", async (req, res) => {
+
+    const { num1, num2, operacion } = req.body;
+
+    let resultado;
+
+    if (operacion === "suma") resultado = num1 + num2;
+    if (operacion === "resta") resultado = num1 - num2;
+    if (operacion === "multiplicacion") resultado = num1 * num2;
+    if (operacion === "division") resultado = num1 / num2;
+
+    const nueva = new Operacion({ num1, num2, operacion, resultado });
+
+    await nueva.save();
+
+    res.json(nueva);
+});
+
+app.delete("/operaciones/:id", async (req,res)=>{
+    await Operacion.findByIdAndDelete(req.params.id);
+    res.json({mensaje:"Operacion eliminada"});
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
